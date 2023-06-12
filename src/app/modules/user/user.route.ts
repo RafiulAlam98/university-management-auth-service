cac8ceb6d6b59fa8b57e1ea5582548b4bfa73d6b
M Rafiul Alam Tonmoy <mrafiul.alam7@gmail.com>
2023-06-12 09:10:31 +0600

    [Cont] zod implementation, middleware created and code refactorise from controller to route

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,9 +1,15 @@
 import express from 'express'
+import { createZodUserSchema } from '../../middlewares/handleZodValidation'
+import { RequestValidation } from '../user.validation'
 import { UserController } from './user.controller'
 
 const router = express.Router()
 
-router.post('/create-user', UserController.createUser)
+router.post(
+  '/create-user',
+  RequestValidation.ValidateRequest(createZodUserSchema),
+  UserController.createUser
+)
 
 export const UserRoutes = {
   router,
