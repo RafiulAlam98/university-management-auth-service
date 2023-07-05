ea1362332ba621489847a01de2b5e1e11215e175
RafiulAlam <mrafiul.alam7@gmail.com>
2023-07-05 23:07:17 +0600

    management department module added

---
+++ src/app/modules/managementDepartment/managementDepartment.route.ts
@@ -0,0 +1,30 @@
+import { ManagementDepartmentController } from './managementDepartment.controller'
+import { ManagementDepartmentValidation } from './managementDepartment.validation'
+import { RequestValidation } from './../../middlewares/validateRequest';
+import express from "express"
+
+const router = express.Router()
+
+router.post(
+  '/create-department',
+  RequestValidation.ValidateRequest(
+    ManagementDepartmentValidation.createManagementDepartmentZodSchema
+  ),
+  ManagementDepartmentController.createDepartment
+)
+
+router.get('/:id', ManagementDepartmentController.getSingleDepartment)
+
+router.patch(
+  '/:id',
+  RequestValidation.ValidateRequest(
+    ManagementDepartmentValidation.updateManagementDepartmentZodSchema
+  ),
+  ManagementDepartmentController.updateDepartment
+)
+
+router.delete('/:id', ManagementDepartmentController.deleteDepartment)
+
+router.get('/', ManagementDepartmentController.getAllDepartments)
+
+export const ManagementDepartmentRoutes = router
